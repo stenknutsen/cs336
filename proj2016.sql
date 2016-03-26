5bd64aecdd1bec3e57a1cc8d33a8e286ef842805
stenknutsen <stenknutsen@gmail.com>
2016-03-26 16:57:04 -0400

    Added table
    game table

--- proj2016.sql
+++ proj2016.sql
@@ -1,1 +1,12 @@
+--TEAM 8
+--Deliverable #3
 --tables for project
+CREATE TABLE game
+(upc_code VARCHAR(12),
+title VARCHAR(50),
+system VARCHAR(20),
+rating VARCHAR(20),
+genre(20)
+PRIMARY KEY (upc_code))
+
+
