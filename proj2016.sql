121011c880cc923a3d6daf58c7c12f415d5690e9
Sten <stenknutsen@gmail.com>
2016-04-01 10:04:25 -0400

    reordered tables
    so references/foreign keys at bottom

--- proj2016.sql
+++ proj2016.sql
@@ -21,19 +21,26 @@ PRIMARY KEY (admin_id),
 FOREIGN KEY (admin_id) REFERENCES account (username) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-DROP TABLE IF EXISTS auction;
-CREATE TABLE auction(
-auction_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-hours_open INT,
-sale_price DECIMAL(10,2),
-auto_sale_price DECIMAL(10,2),
-current_highest_bid DECIMAL(10,2),
-item_of_item_id INT NOT NULL REFERENCES item,
-buy_now_guest_guest_id INT NOT NULL REFERENCES guest,
-cancel_csr_id INT NOT NULL REFERENCES csr_account,
-seller_of_member_id INT NOT NULL REFERENCES member_account 
-); 
+DROP TABLE IF EXISTS csr_account;
+CREATE TABLE csr_account(
+csr_id VARCHAR(20) ,
+maintains_admin_id INT NOT NULL REFERENCES admin_account,
+PRIMARY KEY (csr_id),
+FOREIGN KEY (csr_id) REFERENCES account (username) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+DROP TABLE IF EXISTS member_account;
+CREATE TABLE member_account(
+member_id VARCHAR(20),
+street_address VARCHAR(20),
+city VARCHAR(20),
+zip VARCHAR(20),
+credit_card_number VARCHAR(20),
+paypal_account_number VARCHAR(20),
+--maintains_csr_id INT NOT NULL references csr_account,
+PRIMARY KEY (member_id),
+FOREIGN KEY (member_id) REFERENCES account (username) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
 DROP TABLE IF EXISTS bid;
 CREATE TABLE bid(
@@ -41,53 +48,6 @@ bid_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 time_placed TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 offer DECIMAL(10,2),
 autobid BOOLEAN
-);
-
-DROP TABLE IF EXISTS bid_history;
-CREATE TABLE bid_history(
-bid_id INT NOT NULL REFERENCES bid ON DELETE cascade,
-auction_id INT NOT NULL REFERENCES auction ON DELETE cascade,
-PRIMARY KEY (bid_id, auction_id)
-);
-
-
-
-DROP TABLE IF EXISTS buy_now_guest;
-CREATE TABLE buy_now_guest(
-guest_id INT NOT NULL REFERENCES guest ON DELETE CASCADE,
-auction_id INT NOT NULL REFERENCES auction ON DELETE CASCADE,
-PRIMARY KEY (guest_id, auction_id) 
-);
-
-DROP TABLE IF EXISTS buy_now_member;
-CREATE TABLE buy_now_member(
-member_id INT NOT NULL REFERENCES member_account on delete cascade,
-auction_id INT NOT NULL REFERENCES auction on delete cascade,
-Primary Key (member_id, auction_id) 
-);
-
-DROP TABLE IF EXISTS cart;
-CREATE TABLE cart(
-purchase_id INT NOT NULL,
-has_a_member_id INT NOT NULL REFERENCES member_account,
-has_guest_id INT NOT NULL REFERENCES guest,
-placed_in_item_id INT NOT NULL REFERENCES item,
-PRIMARY KEY (purchase_id)
-);
-
-DROP TABLE IF EXISTS creates;
-CREATE TABLE creates(
-bid_history_id INT NOT NULL references bid_history on delete cascade,
-auction_id INT NOT NULL references auction on delete cascade,
-Primary Key (bid_history_id, auction_id) 
-);
-
-DROP TABLE IF EXISTS csr_account;
-CREATE TABLE csr_account(
-csr_id VARCHAR(20) ,
-maintains_admin_id INT NOT NULL REFERENCES admin_account,
-PRIMARY KEY (csr_id),
-FOREIGN KEY (csr_id) REFERENCES account (username) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 DROP TABLE IF EXISTS game;
@@ -121,6 +81,57 @@ item_condition VARCHAR(20),
 has_unique_upc_code VARCHAR(12) NOT NULL REFERENCES game
 );
 
+DROP TABLE IF EXISTS auction;
+CREATE TABLE auction(
+auction_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+hours_open INT,
+sale_price DECIMAL(10,2),
+auto_sale_price DECIMAL(10,2),
+current_highest_bid DECIMAL(10,2),
+item_of_item_id INT NOT NULL REFERENCES item,
+buy_now_guest_guest_id INT NOT NULL REFERENCES guest,
+cancel_csr_id INT NOT NULL REFERENCES csr_account,
+seller_of_member_id INT NOT NULL REFERENCES member_account 
+); 
+
+DROP TABLE IF EXISTS bid_history;
+CREATE TABLE bid_history(
+bid_id INT NOT NULL REFERENCES bid ON DELETE cascade,
+auction_id INT NOT NULL REFERENCES auction ON DELETE cascade,
+PRIMARY KEY (bid_id, auction_id)
+);
+
+DROP TABLE IF EXISTS buy_now_guest;
+CREATE TABLE buy_now_guest(
+guest_id INT NOT NULL REFERENCES guest ON DELETE CASCADE,
+auction_id INT NOT NULL REFERENCES auction ON DELETE CASCADE,
+PRIMARY KEY (guest_id, auction_id) 
+);
+
+DROP TABLE IF EXISTS buy_now_member;
+CREATE TABLE buy_now_member(
+member_id INT NOT NULL REFERENCES member_account on delete cascade,
+auction_id INT NOT NULL REFERENCES auction on delete cascade,
+Primary Key (member_id, auction_id) 
+);
+
+DROP TABLE IF EXISTS cart;
+CREATE TABLE cart(
+purchase_id INT NOT NULL,
+has_a_member_id INT NOT NULL REFERENCES member_account,
+has_guest_id INT NOT NULL REFERENCES guest,
+placed_in_item_id INT NOT NULL REFERENCES item,
+PRIMARY KEY (purchase_id)
+);
+
+DROP TABLE IF EXISTS creates;
+CREATE TABLE creates(
+bid_history_id INT NOT NULL references bid_history on delete cascade,
+auction_id INT NOT NULL references auction on delete cascade,
+Primary Key (bid_history_id, auction_id) 
+);
+
 DROP TABLE IF EXISTS makes;
 CREATE TABLE makes(
 bid_id INT NOT NULL REFERENCES bid ON DELETE cascade,
@@ -128,15 +139,4 @@ member_id INT NOT NULL REFERENCES member_account ON DELETE cascade,
 PRIMARY KEY (bid_id, member_id)
 );
 
-DROP TABLE IF EXISTS member_account;
-CREATE TABLE member_account(
-member_id VARCHAR(20),
-street_address VARCHAR(20),
-city VARCHAR(20),
-zip VARCHAR(20),
-credit_card_number VARCHAR(20),
-paypal_account_number VARCHAR(20),
---maintains_csr_id INT NOT NULL references csr_account,
-PRIMARY KEY (member_id),
-FOREIGN KEY (member_id) REFERENCES account (username) ON DELETE CASCADE ON UPDATE CASCADE
-);
+
